ddd58b86b27645d5ec336904871290bb428cf802
Gean da Silva lima <geansilvalima@hotmail.com>
2021-07-12 21:39:49 -0300

    26. Criando o ValidationComposite

--- test/validation/validators/email_validation_test.dart
+++ test/validation/validators/email_validation_test.dart
@@ -1,6 +1,18 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:fordev/validation/protocols/protocols.dart';
 
-import 'package:fordev/validation/validators/validators.dart';
+
+class EmailValidation implements FieldValidation{
+   final String field;
+   
+   EmailValidation(this.field);
+   
+   String validate(String value){     
+     final regex = RegExp(r"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})");
+     final isValid = value?.isNotEmpty != true || regex.hasMatch(value);
+     return isValid ? null : 'Campo Inválido.';
+   }
+}
 
 void main(){
 
