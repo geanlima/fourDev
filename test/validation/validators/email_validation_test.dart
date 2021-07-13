c84414f947fb977d7529a11d99b0268bbf2b866f
Gean da Silva lima <geansilvalima@hotmail.com>
2021-07-12 21:13:57 -0300

    24. Criando o RequiredFieldValidation

---
+++ test/validation/validators/email_validation_test.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:fordev/validation/protocols/protocols.dart';
+
+
+class EmailValidation implements FieldValidation{
+   final String field;
+   
+   EmailValidation(this.field);
+   
+   String validate(String value){
+     return null;
+   }
+}
+
+void main(){
+
+  EmailValidation sut;
+
+  setUp((){
+    sut = EmailValidation('any_field');
+  });
+
+  test('Should return null if email is empty',(){
+    expect(sut.validate(''), null);
+ });
+
+ test('Should return null if email is null',(){
+    expect(sut.validate(null), null);
+ });
+}
